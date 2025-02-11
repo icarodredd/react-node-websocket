0a74d7af1ba450720f8b5338471cd3f442c68c1a
icarodredd <icarog854@gmail.com>
2025-02-11 10:32:52 -0300

    feat: receveing message

--- server/index.js
+++ server/index.js
@@ -6,9 +6,26 @@ const uuidv4 = require("uuid").v4;
 const server = http.createServer();
 const wsServer = new WebSocketServer({ server });
 
+const connections = [];
+const users = [];
+
+const handleMessage = (data, uuid) => {
+  const message = JSON.stringify(data.toString());
+  console.log(message);
+};
+
 wsServer.on("connection", (ws, request) => {
   const { username } = url.parse(request.url, true).query;
   const uuid = uuidv4();
+
+  connections[uuid] = ws;
+  users[uuid] = {
+    username,
+    state: {},
+  };
+
+  ws.on("message", (data) => handleMessage(data, uuid));
+  ws.on("close", () => handleClose(uuid));
 
   console.log(username);
   console.log(uuid);
