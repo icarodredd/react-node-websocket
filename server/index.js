76d26f99d3efdeb66b1dac488947747eab5ef025
icarodredd <icarog854@gmail.com>
2025-02-11 10:04:38 -0300

    feat: generating uuid for each user

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 const http = require("http");
 const { WebSocketServer } = require("ws");
 const url = require("url");
+const uuidv4 = require("uuid").v4;
 
 const server = http.createServer();
 const wsServer = new WebSocketServer({ server });
 
 wsServer.on("connection", (ws, request) => {
   const { username } = url.parse(request.url, true).query;
+  const uuid = uuidv4();
+
   console.log(username);
+  console.log(uuid);
 });
 
 server.listen(8080, () => console.log("http://localhost:8080"));
