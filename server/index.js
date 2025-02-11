c2bf5ae17da3be8bec22a7bda6fbb61714324d36
icarodredd <icarog854@gmail.com>
2025-02-11 10:00:48 -0300

    feat: logging the username when the user connect

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const http = require("http");
 const { WebSocketServer } = require("ws");
+const url = require("url");
 
 const server = http.createServer();
 const wsServer = new WebSocketServer({ server });
+
+wsServer.on("connection", (ws, request) => {
+  const { username } = url.parse(request.url, true).query;
+  console.log(username);
+});
 
 server.listen(8080, () => console.log("http://localhost:8080"));
